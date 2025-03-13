23ea5061d3a60fe7ef5b3f27a08b230857f46417
Noelia Grande <nogrande@ucm.es>
2025-03-13 15:31:19 +0100

    Se crea una primera version de la tabla 'observing_run'. Falta la conexión 'muchos a muchos' con la tabla 'members'

--- src/dwarfs4MOSAIC/admin.py
+++ src/dwarfs4MOSAIC/admin.py
@@ -12,7 +12,7 @@ class ObservatoryAdmin(admin.ModelAdmin):
 
     fieldsets = [
         (None, {"fields": ["name"]}),
-        ("Observatory information", {"fields": ["location", "website"]}),
+        ("General Information", {"fields": ["location", "website"]}),
         ("Longitude", {"fields": ["longitude_ew", "longitude_deg", "longitude_min", "longitude_sec"], "classes": ["collapse"]}),
         ("Latitude", {"fields": ["latitude_ns", "latitude_deg", "latitude_min", "latitude_sec"], "classes": ["collapse"]}),
         (None, {"fields": ["altitude"]}),
@@ -25,19 +25,18 @@ admin.site.register(models.Tbl_observatory, ObservatoryAdmin)
 class TelescopeAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {"fields": ["name"]}),
-        ("Telescope information", {"fields": ["description", "owner", "obs_tel", "website", "status"]}),
+        ("General Information", {"fields": ["description", "owner", "obs_tel", "website", "status"]}),
         ("Characteristics", {"fields": ["aperture"]}),
     ]
 
 admin.site.register(models.Tbl_telescope, TelescopeAdmin)
 
 
-
 # 'instrument' table
 class InstrumentAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {"fields": ["name"]}),
-        ("Instrument information", {"fields": ["description", "tel_ins", "website", "status"]}),
+        ("General Information", {"fields": ["description", "tel_ins", "website", "status"]}),
     ]
 
 admin.site.register(models.Tbl_instrument, InstrumentAdmin)
@@ -47,7 +46,32 @@ admin.site.register(models.Tbl_instrument, InstrumentAdmin)
 class MemberAdmin(admin.ModelAdmin):
     fieldsets = [
         (None, {"fields": ["name"]}),
-        ("Member information", {"fields": ["role", "institution", "email"]}),
+        ("General Information", {"fields": ["role", "institution", "email"]}),
     ]
 
 admin.site.register(models.Tbl_member, MemberAdmin)
+
+
+# 'observing_run' table
+class ObservingRunAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("General Information", {"fields": ["name", "description", "start_date", "end_date", ]}),
+        ("Participants", {"fields": ["leader"]}), # "members"
+        ("Additional Data", {"fields": ["notes"]}),
+    ]
+
+admin.site.register(models.Tbl_observing_run, ObservingRunAdmin)
+
+'''
+# 'observing_block' table
+class ObservingBlockAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("General Information", {"fields": ["name", "description", "instrument", "start_date", "start_time", "end_time", ]}),
+        ("Participants", {"fields": ["leader"]}), # "members"
+        ("Observation Information", {"fields": ["target", "observation_mode", "filters", "exposure_time", "seeing", "weather_conditions"]}),  # "members"
+        ("Additional Data", {"fields": ["notes"]}),
+    ]
+
+admin.site.register(models.Tbl_observing_block, ObservingBlockAdmin)
+'''
+
--- src/dwarfs4MOSAIC/models.py
+++ src/dwarfs4MOSAIC/models.py
@@ -14,32 +14,32 @@ class Tbl_observatory(models.Model):
     # fields
     name = models.CharField(
         max_length=200,
-        verbose_name = "Name"
-    )
+        verbose_name = "Name")
+
     website = models.URLField(
         verbose_name = "Website",
-        default=""
-    )
+        default="")
+
     location = models.CharField(
         max_length=200,
         verbose_name="Location",
-        default=""
-    )
+        default="")
+
     longitude = models.FloatField(
         null=True,
         blank=True,
-        validators=[MinValueValidator(-180), MaxValueValidator(180)]
-    )
+        validators=[MinValueValidator(-180), MaxValueValidator(180)])
+
     latitude = models.FloatField(
         null=True,
         blank=True,
-        validators=[MinValueValidator(-90), MaxValueValidator(90)]
-    )
+        validators=[MinValueValidator(-90), MaxValueValidator(90)])
+
     altitude = models.FloatField(
         null=True,
         blank=True,
-        verbose_name="Altitude (m)"
-    )
+        verbose_name="Altitude",
+        help_text="Altitude in meters")
 
     def __str__(self):
         return self.name
@@ -54,40 +54,39 @@ Table 'telescope'
 class Tbl_telescope(models.Model):
     name = models.CharField(
         max_length=200,
-        verbose_name = "Name"
-    )
+        verbose_name = "Name")
+
     description = models.CharField(
         max_length=200,
         null=True,
         blank=True,
-        verbose_name="Description"
-    )
+        verbose_name="Description")
+
     obs_tel = models.ForeignKey( #observatory where the telescope is located
         Tbl_observatory,
         on_delete = models.CASCADE,
-        verbose_name = "Observatory"
-    )
-    owner = models.CharField(
-        max_length=200,
+        verbose_name = "Observatory")
+
+    owner = models.TextField(
         default="",
-        verbose_name = "Institutional Owner",
-    )
+        verbose_name = "Institutional Owner")
+
     aperture = models.FloatField(
-        verbose_name="Aperture (m)",
+        verbose_name="Aperture",
         default=0,
         validators=[MinValueValidator(0)],
-    )
+        help_text="Aperture in meters")
+
     status = models.CharField(
         max_length=20,
         choices=[
             ('unknown', 'Unknown'),
             ('operational', 'Operational'),
             ('inoperative', 'Inoperative'),
-            ('maintenance', 'Under Maintenance'),
-        ],
+            ('maintenance', 'Under Maintenance')],
         default='unknown',
-        verbose_name="Status",
-    )
+        verbose_name="Status")
+
     website = models.URLField(
         verbose_name="Website",
         default="")
@@ -105,30 +104,29 @@ Table 'instrument'
 class Tbl_instrument(models.Model):
     name = models.CharField(
         max_length=200,
-        verbose_name = "Name"
-    )
+        verbose_name = "Name")
+
     description = models.CharField(
         max_length=200,
         null=True,
         blank=True,
-        verbose_name="Description"
-    )
+        verbose_name="Description")
+
     tel_ins = models.ForeignKey( # telescope where the instrument stands
         Tbl_telescope,
         on_delete = models.CASCADE,
-        verbose_name = "Telescope"
-    )
+        verbose_name = "Telescope")
+
     status = models.CharField(
         max_length=20,
         choices=[
             ('unknown', 'Unknown'),
             ('operational', 'Operational'),
             ('inoperative', 'Inoperative'),
-            ('maintenance', 'Under Maintenance'),
-        ],
+            ('maintenance', 'Under Maintenance')],
         default='unknown',
-        verbose_name="Status",
-    )
+        verbose_name="Status")
+
     website = models.URLField(
         verbose_name="Website",
         default="")
@@ -140,34 +138,31 @@ class Tbl_instrument(models.Model):
         verbose_name = "Instrument"
         verbose_name_plural = "Instruments"
 
-
 '''
 Table 'member'
 '''
 class Tbl_member(models.Model):
     name = models.CharField(
         max_length=200,
-        verbose_name="Name"
-    )
+        verbose_name="Name")
+
     role = models.CharField(
         max_length=50,
         choices=[
             ('admin', 'Administrator'),
             ('user', 'User'),
-            ('stuff', 'Stuff'),
-        ],
+            ('stuff', 'Stuff')],
         default='user',
-        verbose_name = "Role"
-    )
+        verbose_name = "Role")
+
     institution = models.CharField(
         max_length=200,
         default="",
-        verbose_name="Institution",
-    )
+        verbose_name="Institution")
+
     email = models.EmailField(
         unique=True,
-        verbose_name="email",
-    )
+        verbose_name="email")
 
     def __str__(self):
         return self.name
@@ -175,3 +170,128 @@ class Tbl_member(models.Model):
     class Meta:
         verbose_name = "Member"
         verbose_name_plural = "Members"
+
+'''
+Table 'observing_run'
+'''
+class Tbl_observing_run(models.Model):
+    name = models.CharField(
+        max_length=200,
+        verbose_name="Name")
+
+    description = models.TextField(
+        null=True,
+        blank=True,
+        verbose_name="Description")
+
+    start_date = models.DateField(
+        verbose_name="Start Date")
+
+    end_date = models.DateField(
+        null=True,
+        blank=True,
+        verbose_name="End Date")
+
+    #members = models.ManyToManyField(Tbl_member)
+
+    leader = models.ForeignKey(
+        Tbl_member,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name='led_observing_runs',
+        verbose_name="Leader")
+
+    #targets = models.ManyToManyField('Target', blank=True)
+
+    notes = models.TextField(
+        blank=True,
+        null=True,
+        verbose_name="Notes")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Observing Run"
+        verbose_name_plural = "Observing Runs"
+
+
+'''
+Table 'observing_block'
+
+class Tbl_observing_block(models.Model):
+    name = models.CharField(
+        max_length=200,
+        verbose_name="Name")
+
+    description = models.TextField(
+        null=True,
+        blank=True,
+        verbose_name="Description")
+        
+    start_date = models.DateField(
+        verbose_name="Start Date")
+        
+    start_time = models.DateField(
+        verbose_name="Start Time")
+        
+    end_time = models.DateField(
+        null=True,
+        blank=True,
+        verbose_name="End Time")
+
+    instrument = models.ForeignKey(
+        Tbl_instrument,
+        on_delete=models.SET_NULL,
+        null=True,
+        verbose_name="Instument")
+
+    #members = models.ManyToManyField(Tbl_member)
+
+    #target = models.ForeignKey(
+        Tbl_target,
+        on_delete=models.SET_NULL,
+        null=True,
+        verbose_name="Target")
+
+    observation_mode = models.CharField(
+        max_length=50,
+        choices=[('photometry', 'Photometry'), ('spectroscopy', 'Spectroscopy'), ('imaging', 'Imaging')],
+        default='photometry',
+        verbose_name="Observation Mode")
+
+    filters = models.CharField(
+        max_length=100,
+        blank=True,
+        null=True,
+        verbose_name="Filters")
+
+    exposure_time = models.DurationField(
+        null=True, 
+        blank=True,
+        verbose_name="Exposure Time")
+        
+    seeing = models.FloatField(
+        null=True, 
+        blank=True, 
+        help_text="Seeing in arcseconds",
+        verbose_name="Seeing")
+        
+    weather_conditions = models.TextField(
+        blank=True, 
+        null=True,
+        verbose_name="Weather Conditions")
+    
+    notes = models.TextField(
+        blank=True,
+        null=True,
+        verbose_name="Notes")
+    
+   def __str__(self):
+        return self.title
+        
+    class Meta:
+        verbose_name = "Observing Block"
+        verbose_name_plural = "Observing Block"
+
+'''
