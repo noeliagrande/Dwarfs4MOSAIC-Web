19f8a95b392510e0594c1afe94fc92ee29822bcb
Noelia Grande <nogrande@ucm.es>
2025-03-30 11:34:57 +0200

    Todas las vistas requieren autenticación

--- src/dwarfs4MOSAIC/urls.py
+++ src/dwarfs4MOSAIC/urls.py
@@ -1,48 +1,49 @@
 from django.urls import path
 from django.contrib.auth.views import LoginView, LogoutView
-from . import views
+from django.contrib.auth.decorators import login_required
+from .views import *
 
 urlpatterns = [
     # Home page
-    path('home/', views.home_view, name='home'),
+    path('home/', home_view, name='home'),
 
     # Login/logout
     path('login/', LoginView.as_view(template_name='admin/login.html'), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
 
     # Database page
-    path('database/', views.database_view, name='database'),
+    path('database/', login_required(database_view), name='database'),
 
     # 'Observatories table' page.
-    path('observatories/', views.observatories_view, name='observatories'),
+    path('observatories/', login_required(observatories_view), name='observatories'),
 
     # Page with information about a specific observatory
-    path('observatory/<str:observatory_name>/', views.observatory_view, name='observatory'),
+    path('observatory/<str:observatory_name>/', login_required(observatory_view), name='observatory'),
 
     # 'Telescopes table' page.
-    path('telescopes/', views.telescopes_view, name='telescopes'),
+    path('telescopes/', login_required(telescopes_view), name='telescopes'),
 
     # Page with information about a specific telescope
-    path('telescope/<str:telescope_name>/', views.telescope_view, name='telescope'),
+    path('telescope/<str:telescope_name>/', login_required(telescope_view), name='telescope'),
 
     # 'Instruments table' page.
-    path('instruments/', views.instruments_view, name='instruments'),
+    path('instruments/', login_required(instruments_view), name='instruments'),
 
     # Page with information about a specific instrument
-    path('instrument/<str:instrument_name>/', views.instrument_view, name='instrument'),
+    path('instrument/<str:instrument_name>/', login_required(instrument_view), name='instrument'),
 
     # 'Researchers table' page.
-    path('researchers/', views.researchers_view, name='researchers'),
+    path('researchers/', login_required(researchers_view), name='researchers'),
 
     # 'Observing_run table' page.
-    path('observing_runs/', views.observing_runs_view, name='observing_runs'),
+    path('observing_runs/', login_required(observing_runs_view), name='observing_runs'),
 
     # Page with information about a specific observing_run
-    path('observing_run/<str:observing_run_name>/', views.observing_run_view, name='observing_run'),
+    path('observing_run/<str:observing_run_name>/', login_required(observing_run_view), name='observing_run'),
 
     # 'Observing_block table' page.
-    path('observing_blocks/', views.observing_blocks_view, name='observing_blocks'),
+    path('observing_blocks/', login_required(observing_blocks_view), name='observing_blocks'),
 
     # 'Target table' page.
-    path('target/', views.targets_view, name='targets'),
+    path('target/', login_required(targets_view), name='targets'),
 ]
--- src/dwarfs4MOSAIC_website/settings.py
+++ src/dwarfs4MOSAIC_website/settings.py
@@ -135,5 +135,6 @@ SESSION_SAVE_EVERY_REQUEST = True # Save the session on every request
 MEDIA_URL = '/media/' # URL to access media files from navigator
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/') # Path where media files are stored (where manage.py is)
 
-LOGIN_URL = 'login'  # Django ‘login’ URL will be used to redirect unauthenticated users
+LOGIN_URL = '/admin/login'  # Django ‘login’ URL will be used to redirect unauthenticated users
+LOGIN_REDIRECT_URL = '/home/'
 LOGOUT_REDIRECT_URL = '/home/'
