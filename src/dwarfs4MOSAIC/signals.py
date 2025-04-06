40b046ff2353c6f067bdee53cbd9f99d64fe058b
Noelia Grande <nogrande@ucm.es>
2025-04-06 10:18:43 +0200

    Grupos y usuarios se actualizan si se modifica el nombre del observatorio

--- src/dwarfs4MOSAIC/signals.py
+++ src/dwarfs4MOSAIC/signals.py
@@ -1,14 +1,39 @@
-from django.db.models.signals import post_save, post_delete
+from django.db.models.signals import pre_save, post_delete
 from django.dispatch import receiver
 from django.contrib.auth.models import Group
 from .models import Tbl_observatory
 
 # Create group when creating observatory
-@receiver(post_save, sender=Tbl_observatory)
-def create_group_for_observatory(sender, instance, created, **kwargs):
-    if created:
-        group_name = instance.name
-        Group.objects.get_or_create(name=group_name)
+@receiver(pre_save, sender=Tbl_observatory)
+def create_or_update_group_for_observatory(sender, instance, **kwargs):
+    try:
+        # if the object already exists (it is not a new object)
+        old_instance = Tbl_observatory.objects.get(pk=instance.pk)
+
+        if old_instance.name != instance.name:
+            # If the observatory's name has changed, we update the group
+            try:
+                old_group = Group.objects.get(name=old_instance.name)
+
+                # Create the new group with the new name
+                new_group, created = Group.objects.get_or_create(name=instance.name)
+
+                # Reasignar usuarios al nuevo grupo
+                users_in_old_group = old_group.user_set.all()
+                for user in users_in_old_group:
+                    user.groups.remove(old_group)  # Remove the user from the old group
+                    user.groups.add(new_group)  # Add the user to the new group
+                    user.save()
+
+                old_group.delete() # Remove old group
+
+            except Group.DoesNotExist:
+                # If the group did not exist, we create it with the new name
+                Group.objects.get_or_create(name=instance.name)
+
+    except Tbl_observatory.DoesNotExist:
+        # If the observatory is new, create the group
+        Group.objects.get_or_create(name=instance.name)
 
 # Delete group when deleting observatory
 @receiver(post_delete, sender=Tbl_observatory)
