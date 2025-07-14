b6c639514ae3606235ba6ddf8a33000c57b2ca9e
Noelia Grande <nogrande@ucm.es>
2025-07-14 13:56:54 +0200

    Nueva funcionalidad: un usuario no administrador puede cambiar su contraseña.

--- src/dwarfs4MOSAIC_website/urls.py
+++ src/dwarfs4MOSAIC_website/urls.py
@@ -19,12 +19,23 @@ from django.urls import include, path
 from django.views.generic import RedirectView
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('django.contrib.auth.urls')),
     path('', RedirectView.as_view(url='/home/', permanent=False)),
     path('', include('dwarfs4MOSAIC.urls')),
+
+    # Change password (user logged)
+    path('accounts/password_change/', auth_views.PasswordChangeView.as_view(), name='password_change'),
+    path('accounts/password_change/done/', auth_views.PasswordChangeDoneView.as_view(), name='password_change_done'),
+
+    # Reset password (user unlogged)
+    path('accounts/password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('accounts/password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('accounts/reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('accounts/reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root= settings.MEDIA_ROOT) # Configured to serve the media files
 #urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
