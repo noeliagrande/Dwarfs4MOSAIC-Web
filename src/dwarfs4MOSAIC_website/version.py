f86cf6a7245dc20bf19e1bb59ff25a7c71fee8e8
Noelia Grande <nogrande@ucm.es>
2025-07-14 18:06:08 +0200

    Añadidos comentarios.

--- src/dwarfs4MOSAIC_website/version.py
+++ src/dwarfs4MOSAIC_website/version.py
@@ -1,5 +1,10 @@
 '''
 Application version (shown in templates and admin footer)
+
+Standard versioning system SemVer (Semantic Versioning):
+- The first digit is incremented when there are changes that are incompatible with previous versions.
+- The second digit is incremented when new features compatible with previous versions are added.
+- The third digit is incremented when bug fixes or minor adjustments are made that do not affect compatibility.
 '''
 
 __version__ = "v2.1.0 (July 2025)"
