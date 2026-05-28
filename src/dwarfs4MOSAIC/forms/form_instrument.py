93848725556086b79f20c2562a8311d08558e4c2
Noelia Grande <nogrande@ucm.es>
2026-05-28 22:10:02 +0200

    Se define el tamaño de los campos usando constants.py

--- src/dwarfs4MOSAIC/forms/form_instrument.py
+++ src/dwarfs4MOSAIC/forms/form_instrument.py
@@ -6,6 +6,14 @@ Admin form for instruments, to set control size.
 from django import forms
 
 # Local application imports
+from ..constants import (
+    CONFIG_AREA,
+    DROPBOX_WIDTH,
+    FILTERS_AREA,
+    NAME_WIDTH,
+    SHORT_DESCRIPTION_WIDTH,
+    URL_WIDTH,
+)
 from ..models import Tbl_instrument
 
 class InstrumentAdminForm(forms.ModelForm):
@@ -14,17 +22,23 @@ class InstrumentAdminForm(forms.ModelForm):
         model = Tbl_instrument
 
         # visual sizes
-        fields = ['description', 'filters', 'configuration', 'website']
+        fields = "__all__"
         widgets = {
-            'description': forms.TextInput(attrs={'size': '75'}),
-            'filters': forms.Textarea(attrs={'rows': 3, 'cols': 30}),
-            'configuration': forms.Textarea(attrs={'rows': 3, 'cols': 30}),
+            'name': forms.TextInput(attrs = {'size': NAME_WIDTH}),
+
+            # General Information
+            'description'   : forms.TextInput(attrs = {'size': SHORT_DESCRIPTION_WIDTH}),
+            'tel_ins'       : forms.Select(attrs = {'style': f'width: {DROPBOX_WIDTH}px;'}),
+            'status'        : forms.Select(attrs = {'style': f'width: {DROPBOX_WIDTH}px;'}),
+
+            # Additional Data
+            'filters'       : forms.Textarea(attrs = FILTERS_AREA),
+            'configuration' : forms.Textarea(attrs = CONFIG_AREA),
         }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        # Visual sizes
-        self.fields['website'].widget.attrs.update({
-            'style': 'width:1000px;'
-        })
+        # Adjust width here to preserve Django's default URLField widget behavior
+        # without overriding the admin URL widget (current/change/clear links).
+        self.fields['website'].widget.attrs.update({'style': f'width: {URL_WIDTH}px;'})
