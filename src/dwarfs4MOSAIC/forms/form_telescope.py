0a6f5cef498d364e57a321aec61f6465f3891016
Noelia Grande <nogrande@ucm.es>
2026-05-28 22:17:57 +0200

    Se define el tamaño de los campos usando constants.py

--- src/dwarfs4MOSAIC/forms/form_telescope.py
+++ src/dwarfs4MOSAIC/forms/form_telescope.py
@@ -6,6 +6,16 @@ Admin form for telescopes, to set control size.
 from django import forms
 
 # Local application imports
+from ..constants import (
+    DROPBOX_OBSERVATORY_WIDTH,
+    DROPBOX_WIDTH,
+    NAME_WIDTH,
+    NUMBER_WIDTH,
+    SHORT_DESCRIPTION_WIDTH,
+    TEXT_AREA,
+    URL_WIDTH,
+)
+
 from ..models import Tbl_telescope
 
 class TelescopeAdminForm(forms.ModelForm):
@@ -14,8 +24,23 @@ class TelescopeAdminForm(forms.ModelForm):
         model = Tbl_telescope
 
         # visual sizes
-        fields = ['owner', 'aperture']
+        fields = "__all__"
         widgets = {
-            'owner': forms.Textarea(attrs={'rows': 3, 'cols': 75}),
-            'aperture': forms.NumberInput(attrs={'style': 'width:80px;', 'min': '0'}),
+            'name': forms.TextInput(attrs = {'size': NAME_WIDTH}),
+
+            # General Information
+            'description'   : forms.TextInput(attrs = {'size': SHORT_DESCRIPTION_WIDTH}),
+            'owner'         : forms.Textarea(attrs = TEXT_AREA),
+            'obs_tel'       : forms.Select(attrs = {'style': f'width: {DROPBOX_OBSERVATORY_WIDTH}px;'}),
+            'status'        : forms.Select(attrs = {'style': f'width: {DROPBOX_WIDTH}px;'}),
+
+            # Characteristics
+            'aperture'      : forms.NumberInput(attrs = {'style': f'width: {NUMBER_WIDTH}px;', 'min': '0'}),
         }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # Adjust width here to preserve Django's default URLField widget behavior
+        # without overriding the admin URL widget (current/change/clear links).
+        self.fields['website'].widget.attrs.update({'style': f'width: {URL_WIDTH}px;'})
